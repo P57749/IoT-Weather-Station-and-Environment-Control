0cce741977314df0030d51bd254c54cf8987a202
Pepe <polopepe567@gmail.com>
2023-07-26 18:43:29 -0400

    Implemented the Light, Lightning, Wind Direction, Wind Speed sensors

--- interface_web_server/script.js
+++ interface_web_server/script.js
@@ -1,26 +1,29 @@
 function updateSensorData(data) {
-    document.getElementById("temperature").textContent = "Temperature: " + data.temperature + " °C";
-    document.getElementById("humidity").textContent = "Humidity: " + data.humidity + " %";
-    document.getElementById("pressure").textContent = "Pressure: " + data.pressure + " Pa";
-    // Update sensor data from the SY300 sensor if available
-  }
-  
-  function fetchSensorData() {
-    fetch("/").then(response => response.json())
-      .then(data => updateSensorData(data))
-      .catch(error => console.error("Error fetching sensor data: ", error));
-  }
-  
-  function sendControlAction(action) {
-    fetch(action).then(response => {
-      if (response.ok) {
-        console.log("Control action sent successfully.");
-      } else {
-        console.error("Failed to send control action.");
-      }
-    }).catch(error => console.error("Error sending control action: ", error));
-  }
-  
-  setInterval(fetchSensorData, 5000); // Fetch sensor data every 5 seconds
-  fetchSensorData(); // Fetch sensor data on page load.
-  
+  document.getElementById("temperature").textContent = "Temperature: " + data.temperature + " °C";
+  document.getElementById("humidity").textContent = "Humidity: " + data.humidity + " %";
+  document.getElementById("pressure").textContent = "Pressure: " + data.pressure + " Pa";
+  document.getElementById("light").textContent = "Light: " + data.light + " lx";
+  document.getElementById("lightning").textContent = "Lightning Distance: " + data.lightning + " km";
+  document.getElementById("wind_direction").textContent = "Wind Direction: " + data.wind_direction + " degrees";
+  document.getElementById("wind_speed").textContent = "Wind Speed: " + data.wind_speed + " Hz";
+  // Update sensor data for rain detection from the 3144 Hall Sensor if available
+}
+
+function fetchSensorData() {
+  fetch("/").then(response => response.json())
+    .then(data => updateSensorData(data))
+    .catch(error => console.error("Error fetching sensor data: ", error));
+}
+
+function sendControlAction(action) {
+  fetch(action).then(response => {
+    if (response.ok) {
+      console.log("Control action sent successfully.");
+    } else {
+      console.error("Failed to send control action.");
+    }
+  }).catch(error => console.error("Error sending control action: ", error));
+}
+
+setInterval(fetchSensorData, 5000); // Fetch sensor data every 5 seconds
+fetchSensorData(); // Fetch sensor data on page load
--- sketch_jul24d.ino
+++ sketch_jul24d.ino
@@ -4,10 +4,15 @@
 #include <DHT.h>
 #include <Adafruit_BMP280.h>
 #include <Wire.h>
-// Include the library for the SY300 sensor if available
 
-const char* ssid = "WIFI_SSID";
-const char* password = "WIFI_PASSWORD";
+// Libraries for additional sensors
+#include <GY30.h> // GY-30 Light Sensor (BH1750)
+#include <AS3935.h> // AS3935 Ray Sensor (Lightning Detector)
+#include <AS5600.h> // AS5600 Hall Sensor (Wind Direction)
+#include <HallSensor.h> // Hall Sensor (Wind Speed and Rain)
+
+const char* ssid = "YOUR_WIFI_SSID";
+const char* password = "YOUR_WIFI_PASSWORD";
 
 #define DHTPIN 2        // DHT22 data pin
 #define DHTTYPE DHT22   // DHT22 sensor type
@@ -16,14 +21,30 @@ DHT dht(DHTPIN, DHTTYPE);
 Adafruit_BMP280 bmp;
 // Initialize the object for the SY300 sensor if available
 
+// Initialize GY-30 Light Sensor
+GY30 gy30;
+
+// Initialize AS3935 Ray Sensor
+AS3935 as3935(0x00); // I2C address 0x00, change it if needed
+
+// Initialize AS5060 Hall Sensor for Wind Direction
+AS5600 as5060;
+
+// Initialize 3144 Hall Sensor for Wind Speed and Rain Detection
+HallSensor hallSensor(13); // GPIO 13 for Wind Speed and Rain Sensor
+
 AsyncWebServer server(80);
 
 void handleRoot() {
   String json = "{";
   json += "\"temperature\": " + String(dht.readTemperature()) + ",";
   json += "\"humidity\": " + String(dht.readHumidity()) + ",";
-  json += "\"pressure\": " + String(bmp.readPressure());
-  // Add the sensor data from the SY300 sensor if available
+  json += "\"pressure\": " + String(bmp.readPressure()) + ",";
+  json += "\"light\": " + String(gy30.readLightLevel()) + ",";
+  json += "\"lightning\": " + String(as3935.getStrikeDistanceKm()) + ",";
+  json += "\"wind_direction\": " + String(as5060.getHeadingDegrees()) + ",";
+  json += "\"wind_speed\": " + String(hallSensor.getPulseFrequency()) + ",";
+  // Add the sensor data for rain detection from the 3144 Hall Sensor if available
   json += "}";
   server.send(200, "application/json", json);
 }
@@ -42,7 +63,7 @@ void setup() {
   Serial.begin(115200);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
+    delay(1000);
     Serial.println("Connecting to WiFi...");
   }
 
@@ -53,6 +74,18 @@ void setup() {
   dht.begin();
   bmp.begin(0x76);
   // Initialize the SY300 sensor if available
+
+  // Initialize GY-30 Light Sensor
+  gy30.begin(BH1750_CONTINUOUS_HIGH_RES_MODE);
+
+  // Initialize AS3935 Ray Sensor
+  as3935.powerUp();
+
+  // Initialize AS5060 Hall Sensor for Wind Direction
+  as5060.begin();
+
+  // Initialize 3144 Hall Sensor for Wind Speed and Rain Detection
+  hallSensor.begin();
 
   server.on("/", HTTP_GET, handleRoot);
   server.on("/on", HTTP_GET, handleOn);
